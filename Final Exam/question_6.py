78511debb6288625d042977c9d55c89cf7964b40
Chauncey Richmond <37638399+ChaunceyRich@users.noreply.github.com>
2019-07-16 00:08:37 -0500

    Update question_6.py

--- Final Exam/question_6.py
+++ Final Exam/question_6.py
@@ -1,3 +1,6 @@
-"""
-Execute Function to Calculate Sum Across Columns?
-"""
+#Question 6
+#Import Numpy Library
+import numpy as np
+#execute Function, create and save new array
+arr_sum = numpy.sum(arr_15, axis=1)
+np.save(arr_sum)
