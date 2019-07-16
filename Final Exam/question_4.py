cd2252e4104f8678653558c15eb50db6dff1aa8e
Chauncey Richmond <37638399+ChaunceyRich@users.noreply.github.com>
2019-07-15 23:36:33 -0500

    Update question_4.py

--- Final Exam/question_4.py
+++ Final Exam/question_4.py
@@ -1,3 +1,10 @@
-"""
-Plot Total Number of Fires For Each Year ?
-"""
+#Question 4
+#Import relevant libraries 
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+%matplotlib inline
+#read data file
+data = pd.read_csv("./Desktop/UCC/CAFires92.csv")
+#group data for plotting 
+fby = data.groupby("year")["fd_unq_id"].count()
